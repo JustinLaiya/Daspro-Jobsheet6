246ad21e84642ef304e53c081b8c4ec4533000c0
JustinLaiya <laiyajustin@gmail.com>
2024-10-05 16:32:04 +0700

    06

---
+++ Pemilihan2percobaan110.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Pemilihan2percobaan110 {
+    public static void main(String[] args) {
+        Scanner input10 = new Scanner(System.in);        
+        System.out.print("masukkan tahun yang ingin di cek: ");
+        int tahun = input10.nextInt();    
+        if (tahun % 400 == 0) {
+            System.out.println("Tahun kabisat");
+        } else {
+            // Cek apakah tahun habis dibagi 100
+            if (tahun % 100 == 0) {
+                System.out.println("Bukan tahun kabisat");
+            } else {
+                // Cek apakah tahun habis dibagi 4
+                if (tahun % 4 == 0) {
+                    System.out.println("Tahun kabisat");
+                } else {
+                    System.out.println("Bukan tahun kabisat");
+                }
+            }
+        }        
+    }
+}
